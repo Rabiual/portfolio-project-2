f3942cfcc2cd879cab4eb422e8773c9b8aeca5d9
Rabiual <rabiualislam1971@gamil.com>
2022-08-22 17:28:16 +0600

    done

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+// -------============ Typing Animation ====================---------
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Web Developer",
+    "Graphic Designer",
+    "youtuber",
+  ],
+  typeSpeed: 200,
+  BackSpeed: 60,
+  loop: true,
+});
+// -------============ Aside ====================---------
+// const nav = document.querySelector(".nav"),
+//   navList = nav.querySelectorAll("li");
+// totalNavLIst = navList.length;
+// for (let i = 0; i < totalNavLIst; i++) {
+//   const a = navList[i].querySelector("a");
+//   a.addEventListener("click", function () {
+//     this.classList.add("active");
+//   });
+// }
